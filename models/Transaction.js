e4530398d377b8920bd2143130bfb73e8c7d5fe6
tommy211369 <tommy.thongrasamy@gmail.com>
2021-08-03 22:15:54 +0200

    add items again

--- models/Transaction.js
+++ models/Transaction.js
@@ -4,6 +4,10 @@ const Transaction = mongoose.model("Transaction", {
   amount: Number,
   currency: String,
   buyer: String,
+  items: {
+    type: mongoose.Schema.Types.Mixed,
+    default: [],
+  },
 });
 
 module.exports = Transaction;
